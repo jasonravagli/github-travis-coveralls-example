b9fa2fca97cc692ee4b2bdca989f7d2254b2790f
jasonravagli <jasonravagli@gmail.com>
2020-05-30 16:08:27 +0200

    Overloaded sayHello method in App class

--- myproject/src/main/java/com/examples/myproject/App.java
+++ myproject/src/main/java/com/examples/myproject/App.java
@@ -4,9 +4,15 @@ package com.examples.myproject;
  * Hello world!
  *
  */
-public class App 
-{
-    public String sayHello() {
-    	return "Hello";
-    }
+public class App {
+	public String sayHello(String name) {
+		if (name == null)
+			return "Hello";
+		else
+			return "Hello " + name;
+	}
+
+	public String sayHello() {
+		return "Hello";
+	}
 }
